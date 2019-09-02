d94e2e5b8caf0747d87268777399341ba2c57890
Alex Modroño <ialex11@engineer.com>
2019-09-02 17:29:45 +0200

    Initial Commit

---
+++ Mamoot!/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Mamoot!
+//
+//  Created by Alex Modroño Vara on 02/09/2019.
+//  Copyright © 2019 Semiak & JoeyPi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
